ef6639c9a5ce5b90ae0e1a57dea094184e73da67
Seungyeon <122261047+989ksy@users.noreply.github.com>
2023-11-23 21:26:27 +0900

    [init] Commit

---
+++ Five/Five/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Five
+//
+//  Created by Seungyeon Kim on 11/23/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
