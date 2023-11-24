6307447e1c6d4bc3c44d0c733223024f8e7cc8b7
Seungyeon <122261047+989ksy@users.noreply.github.com>
2023-11-25 00:29:57 +0900

    [Feat] Email 중복확인 구현

--- Five/Five/SignUp/Email/EmailViewController.swift
+++ Five/Five/SignUp/Email/EmailViewController.swift
@@ -28,19 +28,7 @@ class EmailViewController: BaseViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
-                        
-        APIManager.shared.emailValidation(email: "test565@naver.com")
-            .subscribe { event in
-                switch event {
-                case .success(let result) :
-                    print("validatoin ok : \(result)")
-                case .failure( let error):
-                    print("validation fail : \(error)")
-                }
-            }
-            .disposed(by: disposeBag)
 
-        
         setNavigation()
         bind()
     }
@@ -52,79 +40,40 @@ class EmailViewController: BaseViewController {
     
     func bind() {
         
-        let input = EmailViewModel.Input(emailText: mainView.emailTextfield.rx.text.orEmpty, tap: mainView.nextButton.rx.tap)
+        let input = EmailViewModel.Input(emailText: mainView.emailTextfield.rx.text.orEmpty, emailValidationTap: mainView.validationButton.rx.tap, nextTap: mainView.nextButton.rx.tap)
+        
         let output = viewModel.transform(input: input)
         
-        viewModel.directionText
+        output.directionText
             .asDriver()
             .drive(self.mainView.directionLabel.rx.text)
             .disposed(by: disposeBag)
         
-        output.validation
-            .bind(to: self.mainView.nextButton.rx.isEnabled, self.mainView.directionLabel.rx.isHidden)
+        output.emailValidation
+            .bind(to: self.mainView.nextButton.rx.isEnabled)
             .disposed(by: disposeBag)
         
-        output.validation
+        output.emailValidation
             .subscribe(with: self) { owner, value in
                 owner.mainView.nextButton.backgroundColor = value ? CustomColor.pointColor : .lightGray
                 owner.mainView.nextButton.isEnabled = value
+                print("===emailValidation 버튼: \(value)")
+                
             }
             .disposed(by: disposeBag)
-        
-        /*
-         - 이메일 입력
-         - validationCheck -> "이미 가입된 이메일입니다"
-         - check되면 버튼 => 활성화 => 화면전환 & 값전달
-         */
-        
-//        output.responseText
-//            .bind(to: mainView.directionLabel.rx.text)
-//            .disposed(by: disposeBag)
-        
-//        output.responseText
-//            .subscribe(with: self) { owner, text in
-//                owner.mainView.
-//            }
-//            .disposed(by: disposeBag)
-        
-//        output.responseText
-//            .subscribe {
-//                if $0 == "성공" {
-//                    .push
-//                } else {
-//                    tex.text = "뭐가 안되서 안되는 겁니다"
-//                }
-//            }
-        
-        output.tap
-            .throttle(.seconds(1), scheduler: MainScheduler.instance)
-            .withLatestFrom(input.emailText, resultSelector: { _, query in
-                return query
-            })
-            .map { query in
-                return "\(query)" }
-            .flatMap{
-                APIManager.shared.emailValidation(email: $0)
-            }
-            .subscribe(with: self) { owner, result in
-
-                
-                
+          
+        input.nextTap
+            .subscribe(with: self) { owner, _ in
+                let vc = PasswordViewController()
+                guard let emailInput = owner.mainView.emailTextfield.text else {return }
+                vc.email = emailInput
+                self.navigationController?.pushViewController(vc, animated: true)
             }
             .disposed(by: disposeBag)
         
         
-//            .bind { _ in
-//
-//                /// statusCode 200
-//                let vc = PasswordViewController()
-//                vc.email = "\(input.emailText)"
-//
-//                //화면전환 -> PasswordVC
-//                self.navigationController?.pushViewController(vc, animated: true)
-//            }
-//            .disposed(by: disposeBag)
     }
+    
     
     
     //MARK: - NavigationController Setting
@@ -139,3 +88,4 @@ class EmailViewController: BaseViewController {
     
 }
 
+        
--- Five/Five/SignUp/Email/EmailViewModel.swift
+++ Five/Five/SignUp/Email/EmailViewModel.swift
@@ -11,35 +11,70 @@ import RxCocoa
 
 class EmailViewModel {
     
-    let directionText = BehaviorRelay(value: "@을 포함한 이메일 주소를 입력해주세요.")
-    
     let emailRegex = #"^([a-zA-Z0-9._-])+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,20}$"#
+    
+    let disposeBag = DisposeBag()
     
     struct Input {
         let emailText : ControlProperty<String>
-        let tap : ControlEvent<Void>
+        let emailValidationTap : ControlEvent<Void>
+        let nextTap : ControlEvent<Void>
     }
     
     struct Output {
-        let tap : ControlEvent<Void>
-        let responseText : BehaviorRelay<String>
-        let validation : Observable<Bool>
+        let emailValidation : Observable<Bool>
+        let isSucceeded : PublishSubject<Bool>
+        let directionText : BehaviorRelay<String>
     }
     
     func transform (input: Input) -> Output {
         
-        let validation = input.emailText
+        //이메일 정규식 확인
+        let emailTypeValidation = input.emailText
             .map{ $0.range(of: self.emailRegex, options: .regularExpression) != nil}
         
-        let message = BehaviorRelay(value: String())
+        let isSucceeded = PublishSubject<Bool>()
+        let directionText = BehaviorRelay(value: "@을 포함한 이메일 주소를 입력해주세요.")
         
-//        input.tap
-//            .withLatestFrom()
-//            .flatMap(<#T##selector: (_) throws -> ObservableConvertibleType##(_) throws -> ObservableConvertibleType#>)
-//            .subscribe(<#T##observer: ObserverType##ObserverType#>) // bool
+        //네트워크 통신
+        input.emailValidationTap
+            .throttle(.seconds(1), scheduler: MainScheduler.instance)
+            .withLatestFrom(input.emailText, resultSelector: { _, query in
+                return query
+            })
+            .map { query in
+                return "\(query)" }
+            .flatMap{
+                APIManager.shared.emailValidation(email: $0)
+            }
+            .subscribe(with: self) { owner, result in
+                switch result {
+                case .success(let response):
+                    print(response.message)
+                    isSucceeded.onNext(true)
+                    directionText.accept(response.message)
+                case .failure(let failure):
+                    print(failure.localizedDescription)
+                    isSucceeded.onNext(false)
+                    directionText.accept(failure.localizedDescription)
+                }
+            }
+            .disposed(by: disposeBag)
         
+        // 정규식 + 네트워크 true일 때
+        let nextValidation = Observable.combineLatest (emailTypeValidation, isSucceeded)
+            .map { email, success in
+                return email && success
+            } //zip..!!!
         
-        return Output (tap: input.tap, responseText: message, validation: validation)
+        // nextValidation 디버깅
+        nextValidation
+            .subscribe(with: self) { owner, isValid in
+                print("Next Validation:", isValid)
+            }
+            .disposed(by: disposeBag)
+        
+        return Output (emailValidation: nextValidation, isSucceeded: isSucceeded, directionText: directionText)
     }
     
     
