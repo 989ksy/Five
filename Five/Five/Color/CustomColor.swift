353b95d69610039d8201b8d62a3d93fe596bccb6
Seungyeon <122261047+989ksy@users.noreply.github.com>
2023-11-24 13:52:47 +0900

    [Feat] CustomColor (#FFB100)

---
+++ Five/Five/Color/CustomColor.swift
@@ -0,0 +1,14 @@
+//
+//  CustomColor.swift
+//  Five
+//
+//  Created by Seungyeon Kim on 11/24/23.
+//
+
+import UIKit
+
+enum CustomColor {
+    
+    static let pointColor = UIColor(named: "PointColor")
+    
+}
