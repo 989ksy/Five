fd9998401456a1abb76b2f0600722cdb6cffd124
Seungyeon <122261047+989ksy@users.noreply.github.com>
2023-11-26 23:00:08 +0900

    [Design] navigationController 설정 수정

--- Five/Five/Feed/FeedViewController.swift
+++ Five/Five/Feed/FeedViewController.swift
@@ -22,6 +22,10 @@ class FeedViewController : BaseViewController {
         super.viewDidLoad()
         view.backgroundColor = .white
         
+        mainView.feedCollectionView.delegate = self
+        mainView.feedCollectionView.dataSource = self
+
+        
         setNavigationController()
     }
     
@@ -32,14 +36,28 @@ class FeedViewController : BaseViewController {
         title = "Five"
         
         //로고 UI 설정
-        self.navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.font : CustomFont.logo25 ?? .boldSystemFont(ofSize: 25), NSAttributedString.Key.foregroundColor:CustomColor.pointColor ?? .systemYellow]
+        self.navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.font : CustomFont.logo25 ?? .boldSystemFont(ofSize: 24), NSAttributedString.Key.foregroundColor:CustomColor.pointColor ?? .systemYellow]
         
         //네비게이션 컬러
-        self.navigationController?.navigationBar.tintColor = .black//Color.pointColor
+        self.navigationController?.navigationBar.tintColor = .black
 
-        
+
         
     }
     
 }
 
+
+extension FeedViewController : UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "FeedCollectionViewCell", for: indexPath) as? FeedCollectionViewCell else {return UICollectionViewCell()}
+        
+        return cell
+    }
+    
+    
+}
