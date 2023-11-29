4db7f352c951d90631cd7e7cc002a4a34c1ee8c1
Seungyeon <122261047+989ksy@users.noreply.github.com>
2023-11-29 20:16:54 +0900

    [Create] 탭바 테스트용 ProfileVC

---
+++ Five/Five/Profile/ProfileViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ProfileViewController.swift
+//  Five
+//
+//  Created by Seungyeon Kim on 11/29/23.
+//
+
+import UIKit
+
+class ProfileViewController: BaseViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
