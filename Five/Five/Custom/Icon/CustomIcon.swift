863ab37305a789f7683e8af521fffb9e0d031742
Seungyeon <122261047+989ksy@users.noreply.github.com>
2023-11-29 20:17:58 +0900

    [Add] ImageAssets 아이콘 새로 추가

---
+++ Five/Five/Custom/Icon/CustomIcon.swift
@@ -0,0 +1,15 @@
+//
+//  CustomIcon.swift
+//  Five
+//
+//  Created by Seungyeon Kim on 11/26/23.
+//
+
+import UIKit
+
+enum CustomIcon {
+    
+    static let addIcon = UIImage(systemName: "add")
+    static let moreIcon = UIImage(systemName: "more")
+    
+}
