0ddc58e1852843ab4876d67c7c15f544e6fe209f
Seungyeon <122261047+989ksy@users.noreply.github.com>
2023-12-01 23:53:33 +0900

    [Create] SearchVC

---
+++ Five/Five/Search/SearchViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SearchViewController.swift
+//  Five
+//
+//  Created by Seungyeon Kim on 12/1/23.
+//
+
+import UIKit
+
+class SearchViewController: BaseViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+    
+    
+}
