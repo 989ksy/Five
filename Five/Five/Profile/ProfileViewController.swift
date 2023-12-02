b6b722a0efeeb32845546736547bd166c627b874
Seungyeon <122261047+989ksy@users.noreply.github.com>
2023-12-02 23:45:59 +0900

    [Feat] RxSwift tap, 프로필->설정 화면전환

--- Five/Five/Profile/ProfileViewController.swift
+++ Five/Five/Profile/ProfileViewController.swift
@@ -6,11 +6,43 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
-class ProfileViewController: BaseViewController {
+final class ProfileViewController: BaseViewController {
+    
+    let mainView = ProfileView()
+    let viewModel = ProfileViewModel()
+    
+    let disposeBag = DisposeBag()
+    
+//    private var viewControllers = [MyFiveViewController(), FivedViewController()]
+        
+    override func loadView() {
+        self.view = mainView
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        bind()
     }
+    
+    
+    func bind() {
+        
+        let input = ProfileViewModel.Input(settingTap: mainView.settingButton.rx.tap)
+        
+        mainView.settingButton.rx.tap
+            .subscribe(with: self) { owner, _ in
+                let vc = SettingViewController()
+                self.navigationController?.pushViewController(vc, animated: true)
+            }
+            .disposed(by: disposeBag)
+        
+    }
+    
+    
+    
     
 }
---
+++ Five/Five/Profile/ProfileViewModel.swift
@@ -0,0 +1,26 @@
+//
+//  ProfileViewModel.swift
+//  Five
+//
+//  Created by Seungyeon Kim on 12/2/23.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+
+class ProfileViewModel {
+    
+    struct Input {
+        
+        let settingTap : ControlEvent<Void>
+        
+        
+    }
+    
+    struct Output {
+        
+    }
+    
+    
+}
