b96942021a23b78fbdd4f216c31296fdd062b76e
Seungyeon <122261047+989ksy@users.noreply.github.com>
2023-12-02 23:20:10 +0900

    [Feat] 탭 시 키보드 내림 extension

---
+++ Five/Five/Extension + Share/HideKeyboard+Extension.swift
@@ -0,0 +1,20 @@
+//
+//  HideKeyboard+Extension.swift
+//  Five
+//
+//  Created by Seungyeon Kim on 12/2/23.
+//
+
+import UIKit
+
+extension UIViewController {
+    func hideKeyboardWhenTappedAround() {
+        let tap = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+}
