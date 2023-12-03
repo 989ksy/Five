02a58d1e605e2b665a10211be3f6b005cf117bd6
Seungyeon <122261047+989ksy@users.noreply.github.com>
2023-12-03 20:56:35 +0900

    [Create] FiveVC, FivedVC 생성

---
+++ Five/Five/Profile/Five/FiveViewController.swift
@@ -0,0 +1,17 @@
+//
+//  FiveViewController.swift
+//  Five
+//
+//  Created by Seungyeon Kim on 12/1/23.
+//
+
+import UIKit
+
+class FiveViewController : BaseViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+    
+}
---
+++ Five/Five/Profile/Fived/FivedViewController.swift
@@ -0,0 +1,18 @@
+//
+//  FivedViewController.swift
+//  Five
+//
+//  Created by Seungyeon Kim on 12/1/23.
+//
+
+import UIKit
+
+final class FivedViewController : BaseViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+    
+    
+}
