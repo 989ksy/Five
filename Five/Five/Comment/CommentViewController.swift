b59787b833f892729176dacfcdcbc4f9a91b196b
Seungyeon <122261047+989ksy@users.noreply.github.com>
2023-12-03 20:56:09 +0900

    [Feat] UISegmentedControl UI 설정

---
+++ Five/Five/Comment/CommentViewController.swift
@@ -0,0 +1,16 @@
+//
+//  CommentViewController.swift
+//  Five
+//
+//  Created by Seungyeon Kim on 12/3/23.
+//
+
+import UIKit
+
+final class CommentViewController : BaseViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
