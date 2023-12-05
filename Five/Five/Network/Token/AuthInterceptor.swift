1455fcade3f479344bdccd7c00bd978dc5c6ce6c
Seungyeon <122261047+989ksy@users.noreply.github.com>
2023-12-05 17:08:23 +0900

    [Feat] refreshToken 갱신 실패시 로그인화면으로 전환

--- Five/Five/Network/Token/AuthInterceptor.swift
+++ Five/Five/Network/Token/AuthInterceptor.swift
@@ -19,6 +19,7 @@ final class AuthInterceptor: RequestInterceptor {
     private init() {}
 
     //adapt를 통해 Request Header에 필요한 토큰을 넣기
+    //adapt: request전에 특정 작업을 하고 싶을 때
     func adapt(_ urlRequest: URLRequest, for session: Session, completion: @escaping (Result<URLRequest, Error>) -> Void) {
         guard urlRequest.url?.absoluteString.hasPrefix(BaseURL.base) == true,
               let accessToken = KeychainStorage.shared.userToken,
@@ -36,6 +37,7 @@ final class AuthInterceptor: RequestInterceptor {
     }
 
     //response의 statusCode가 419인 경우 토큰을 갱신하는 API를 호출
+    //retry: 요청을 재시도 해야하는지 여부를 결정하는 메서드
     func retry(_ request: Request, for session: Session, dueTo error: Error, completion: @escaping (RetryResult) -> Void) {
         print("retry 진입")
         guard let response = request.task?.response as? HTTPURLResponse, response.statusCode == 419
@@ -51,11 +53,23 @@ final class AuthInterceptor: RequestInterceptor {
             .subscribe(with: self) { owner, result in
                 switch result {
                 case .success(let response):
-                    print("Retry-토큰 재발급 성공")
+                    print("Retry-토큰 재발급 성공 = 갈아끼우기")
                     KeychainStorage.shared.userToken = response.token
+                    completion(.retry)
+                    
                 case .failure(let error):
                     //갱신 실패 -> 로그인 화면으로 전환
+                    
                     completion(.doNotRetryWithError(error))
+                    
+                    let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene
+                    let sceneDelegate = windowScene?.delegate as? SceneDelegate
+                    
+                    let vc = LoginViewController()
+                    let nav = UINavigationController(rootViewController: vc)
+                    
+                    sceneDelegate?.window?.rootViewController = nav
+                    sceneDelegate?.window?.makeKeyAndVisible()
                 }
             }
             .disposed(by: disposeBag)
