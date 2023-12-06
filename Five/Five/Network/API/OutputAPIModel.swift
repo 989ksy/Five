8378638cb64639e32294f5ac3ccba0f3f7d8d014
Seungyeon <122261047+989ksy@users.noreply.github.com>
2023-12-06 18:48:14 +0900

    [Feat] CreatePostResponse, Creator in APIModel

--- Five/Five/Network/API/OutputAPIModel.swift
+++ Five/Five/Network/API/OutputAPIModel.swift
@@ -34,3 +34,30 @@ struct CheckEmailResponse : Decodable {
 struct RefreshTokenResponse : Decodable {
     let token : String
 }
+
+//MARK: - 포스트 작성
+
+struct CreatePostResponse: Decodable {
+
+    let id, time, content, productId : String
+    let likes, image, hashTags : [String]
+    let creator: Creator
+    
+    enum CodingKeys: String, CodingKey {
+        case likes, image, hashTags
+        case id = "_id"
+        case time, content, creator
+        case productId = "product_id"
+    }
+}
+
+struct Creator: Decodable {
+    
+    let id, nick : String
+    
+    enum CodingKeys: String, CodingKey {
+        case id = "_id"
+        case nick
+    }
+    
+}
