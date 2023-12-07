cddc1c0486e70924a157445f9d587b6e88788f51
Seungyeon <122261047+989ksy@users.noreply.github.com>
2023-12-07 19:45:43 +0900

    [Rename] 폴더 세분화 정리

---
+++ Five/Five/Extension + Share/UIButton/FollowUIButton.swift
@@ -0,0 +1,30 @@
+//
+//  FollowUIButton.swift
+//  Five
+//
+//  Created by Seungyeon Kim on 12/7/23.
+//
+
+import UIKit
+
+class FollowUIButton : UIButton {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        set()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func set() {
+        layer.cornerRadius = 6
+        layer.borderWidth = 1
+        layer.borderColor = UIColor.systemGray4.cgColor
+        titleLabel?.font = CustomFont.mediumGmarket15
+        setTitleColor(.darkGray, for: .normal)
+        setTitle("팔로우하기", for: .normal)
+    }
+    
+}
