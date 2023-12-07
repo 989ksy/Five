abb1004d2621afe23e808c92dfeb825ea814b0c2
Seungyeon <122261047+989ksy@users.noreply.github.com>
2023-12-07 19:45:58 +0900

    [Create] BaseTableViewCell

---
+++ Five/Five/Base/Cell/BaseTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  BaseTableViewCell.swift
+//  Five
+//
+//  Created by Seungyeon Kim on 12/7/23.
+//
+
+import UIKit
+
+class BaseTableViewCell: UITableViewCell {
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        configureView()
+        setConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configureView() {}
+    
+    func setConstraints() {}
+}
