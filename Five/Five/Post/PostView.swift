c28f879d135481ee3bc19d09dbc401b2c1d4e2b3
Seungyeon <122261047+989ksy@users.noreply.github.com>
2023-12-08 22:19:01 +0900

    [Create] PostView, PostVC

---
+++ Five/Five/Post/PostView.swift
@@ -0,0 +1,21 @@
+//
+//  PostView.swift
+//  Five
+//
+//  Created by Seungyeon Kim on 12/7/23.
+//
+
+import UIKit
+
+class PostView : BaseView {
+    
+    override func configureView() {
+        
+    }
+    
+    override func setConstraints() {
+        
+    }
+    
+    
+}
---
+++ Five/Five/Post/PostViewController.swift
@@ -0,0 +1,21 @@
+//
+//  PostViewController.swift
+//  Five
+//
+//  Created by Seungyeon Kim on 12/7/23.
+//
+
+import UIKit
+
+class PostViewController : BaseViewController {
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    
+    
+}
