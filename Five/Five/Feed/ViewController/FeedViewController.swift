097d3b40293ff7f6bcc9a10651babd0cf69f6d47
Seungyeon <122261047+989ksy@users.noreply.github.com>
2023-12-13 21:43:13 +0900

    [Feat] FeedVC Pagenation

--- Five/Five/Feed/ViewController/FeedViewController.swift
+++ Five/Five/Feed/ViewController/FeedViewController.swift
@@ -21,6 +21,7 @@ final class FeedViewController : BaseViewController, UISheetPresentationControll
     
     var refresh = PublishSubject<Void>()
     
+    //리프레싱 컨트롤 생성
     private lazy var refreshControl: UIRefreshControl = {
         let refreshControl = UIRefreshControl()
         refreshControl.addTarget(self, action: #selector(refreshingData), for: .valueChanged)
@@ -36,19 +37,27 @@ final class FeedViewController : BaseViewController, UISheetPresentationControll
         super.viewDidLoad()
         view.backgroundColor = CustomColor.backgroundColor
         
-        setNavigationController()
-        bind()
+        setNavigationController() //네비게이션바 세팅
+        bind() // rx
         
+        //리프레싱
         mainView.feedCollectionView.refreshControl = refreshControl
         
-                
+        NotificationCenter.default.addObserver(self, selector: #selector(uploadView), name: NSNotification.Name("contentUploaded"), object: nil)
+        
+        
     }
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        
-        mainView.feedCollectionView.reloadData()
-        
+    //    override func viewWillAppear(_ animated: Bool) {
+    //        super.viewWillAppear(animated)
+    //
+    //        refresh.onNext(Void())
+    //    }
+    
+    
+    //MARK: - 새 포스트 갱신
+    
+    @objc func uploadView() {
         refresh.onNext(Void())
     }
     
@@ -56,24 +65,21 @@ final class FeedViewController : BaseViewController, UISheetPresentationControll
     //MARK: - 화면 로딩
     
     @objc private func refreshingData() {
-
         refreshControl.endRefreshing()
-        
-        print("refreshing view")
-        
         refresh.onNext(Void())
-        
-
-   }
+    }
     
-
+    
     //MARK: - Bind
     
     func bind() {
         
-        let input = FeedViewModel.Input(addContentButtonTap: mainView.addContentButton.rx.tap, refresh: refresh)
+        let input = FeedViewModel.Input(
+            addContentButtonTap: mainView.addContentButton.rx.tap,
+            refresh: refresh,
+            prefetchItem: mainView.feedCollectionView.rx.prefetchItems
+        )
         let output = viewModel.transform(input: input)
-        
         
         //작성하기 버튼 작동
         input.addContentButtonTap
@@ -84,96 +90,89 @@ final class FeedViewController : BaseViewController, UISheetPresentationControll
             }
             .disposed(by: disposeBag)
         
+        //MARK: - FeedCollectionViewCell
         
+        //CellForItem
         output.items
             .observe(on: MainScheduler.instance)
-            .bind(to: mainView.feedCollectionView.rx.items(cellIdentifier: "FeedCollectionViewCell", cellType: FeedCollectionViewCell.self)) { (row, element, cell) in
+            .bind(
+                to: mainView.feedCollectionView.rx.items(
+                    cellIdentifier: "FeedCollectionViewCell",
+                    cellType: FeedCollectionViewCell.self)
+            ) { (row, element, cell) in
                 
                 //닉네임
                 cell.nickLabel.text = "\(element.creator.nick)"
                 
                 //날짜
-                let dateString = "\(element.time)"
-
-                let dateFormatter = DateFormatter()
-                dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZ"
-                dateFormatter.locale = Locale(identifier: "en_US_POSIX")
-
-                if let date = dateFormatter.date(from: dateString) {
-                    
-                    let formattedDate = DateFormatter.localizedString(from: date, dateStyle: .medium, timeStyle: .none)
-                    cell.dateLabel.text = formattedDate
-                    
-                } else {
-                    print("유효하지 않은 날짜 형식: \(dateString)")
-                }
+                cell.dateLabel.customDateFormat(initialText: element.time)
                 
                 //사진
                 let url = URL(string: "\(BaseURL.base)" + element.image.first!)
                 cell.imageView.loadImage(from: url!, placeHolderImage: UIImage(named: "personal"))
                 
-            }
-            .disposed(by: disposeBag)
-        
-        
-        output.nextCursor
-            .subscribe(with: self) { owner, value in
-                owner.mainView.feedCollectionView.reloadData()
-            }
-            .disposed(by: disposeBag)
-
-        
-        mainView.feedCollectionView.rx.itemSelected
-            .subscribe(with: self) { owner, indexPath in
-
-                let vc = PostViewController()
-                owner.navigationController?.pushViewController(vc, animated: true)
+                //댓글 버튼
+                cell.commentButton
+                    .rx
+                    .tap
+                    .subscribe(with: self) { owner, _ in
+                        let vc = CommentViewController()
+                        vc.modalPresentationStyle = .pageSheet
+                        
+                        if let sheet = vc.sheetPresentationController {
+                            //지원할 크기 지정
+                            sheet.detents = [.medium(), .large()]
+                            //크기 변하는거 감지
+                            sheet.delegate = self
+                            
+                            //시트 상단에 그래버 표시 (기본 값은 false)
+                            sheet.prefersGrabberVisible = true
+                        }
+                        
+                        self.present(vc, animated: true, completion: nil)
+                    }
+                    .disposed(by: cell.disposeBag)
                 
+                //닉네임 버튼
+                cell.nicknameButton
+                    .rx
+                    .tap
+                    .subscribe(with: self) { owner, _ in
+                        let vc = ProfileViewController()
+                        self.navigationController?.pushViewController(vc, animated: true)
+                    }
+                    .disposed(by: cell.disposeBag)
             }
             .disposed(by: disposeBag)
         
+        //CellDidSelected
+        mainView.feedCollectionView
+            .rx
+            .modelSelected(ReadData.self)
+            .observe(on: MainScheduler.instance)
+            .subscribe(with: self) { owner, data in
+                let vc = PostViewController()
+                vc.transitedData.accept(data)
+                
+                vc.mainView.nickLabel.text = data.creator.nick
+                vc.mainView.contentLabel.text = data.content
+
+                self.navigationController?.pushViewController(vc, animated: true)
+            }
+            .disposed(by: disposeBag)
         
-        refreshControl.rx.controlEvent(.valueChanged)
+        //CollectionView 갱신
+        refreshControl
+            .rx
+            .controlEvent(.valueChanged)
             .subscribe(with: self) { owner, _ in
                 owner.mainView.feedCollectionView.reloadData()
                 owner.refreshControl.endRefreshing()
             }
             .disposed(by: disposeBag)
         
-        
     }
     
-    
-    //MARK: - 닉네임 버튼
-    
-    @objc func nicknameButtonTapped() {
-        let vc = ProfileViewController()
-        navigationController?.pushViewController(vc, animated: true)
-    }
-    
-    
-    //MARK: - 댓글 버튼
-    
-    @objc func commentButtonTapped() {
-        
-        let vc = CommentViewController()
-        vc.modalPresentationStyle = .pageSheet
-        
-        if let sheet = vc.sheetPresentationController {
-            
-            //지원할 크기 지정
-            sheet.detents = [.medium(), .large()]
-            //크기 변하는거 감지
-            sheet.delegate = self
-            
-            //시트 상단에 그래버 표시 (기본 값은 false)
-            sheet.prefersGrabberVisible = true
-            
-        }
-        
-        present(vc, animated: true, completion: nil)
-        
-    }
     
     //MARK: - navigation+item
     
@@ -182,9 +181,9 @@ final class FeedViewController : BaseViewController, UISheetPresentationControll
         //타이틀 설정(로고설정)
         let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 20, height: 15))
         imageView.contentMode = .scaleAspectFit
-            let image = UIImage(named: "logo")
-            imageView.image = image
-            navigationItem.titleView = imageView
+        let image = UIImage(named: "logo")
+        imageView.image = image
+        navigationItem.titleView = imageView
         
         self.navigationController?.navigationBar.topItem?.title = ""
         self.navigationController?.navigationBar.tintColor = .black
@@ -202,27 +201,7 @@ final class FeedViewController : BaseViewController, UISheetPresentationControll
     }
     
     
-    //MARK: - 스크롤 시 네비게이션바 숨기기
-    
-    //사용자가
-    func scrollViewWillBeginDragging(_ scrollView: UIScrollView) {
-        self.navigationController?.setNavigationBarHidden(true, animated: true)
-        
-    }
-    
-    //decelerate : 스크롤 인지 bool값;
-    //사용자가 스크롤을 멈추면 false.
-    func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
-        if decelerate == false {
-            self.navigationController?.setNavigationBarHidden(false, animated: true)
-        }
-    }
-    
-    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
-        self.navigationController?.setNavigationBarHidden(false, animated: true)
-        
-    }
-
     
     
 }
+
--- Five/Five/Feed/ViewModel/FeedViewModel.swift
+++ Five/Five/Feed/ViewModel/FeedViewModel.swift
@@ -10,7 +10,7 @@ import RxSwift
 import RxCocoa
 
 class FeedViewModel {
-    
+
     let disposeBag = DisposeBag()
 
     struct Input {
@@ -18,33 +18,77 @@ class FeedViewModel {
         //버튼
         let addContentButtonTap : ControlEvent<Void> //글작성버튼
         let refresh: PublishSubject<Void>
+        
+        let prefetchItem: ControlEvent<[IndexPath]>
+        
     }
     
     struct Output {
-        
-        let items: PublishSubject<[ReadData]>
-        let nextCursor: BehaviorSubject<String>
-        
+        let items: BehaviorSubject<[ReadData]>
     }
     
     func transform (input: Input) -> Output {
         
-        let FeedData = PublishSubject<[ReadData]>()
+        let feedData = BehaviorSubject<[ReadData]>(value: [])
+
+        //MARK: - 페이지네이션
         
-        var nextCursor = BehaviorSubject(value: "")
-        let value = nextCursor.map{ $0 }
+        // 페이지네이션 요소
+        let nextCursor = BehaviorSubject<String>(value: "") // 지금
+        var nextCursorItem = "" // 미리 커서 저장 , 페이지네이션에 사용할 값
+        
+        
+        //프리페칭해!
+        //조건: 배열의 마지막쯤이 되었을 때 = 네트워크 콜
+        input.prefetchItem
+            .subscribe(with: self) { owner , value in
+                let itemCount = try! feedData.value().count
                 
+                if value.contains(where: { $0.row == itemCount - 1 }) {
+                    if nextCursorItem != "0" {
+                        nextCursor.onNext(nextCursorItem)
+                    }
+                }
+            }
+            .disposed(by: disposeBag)
+        
+        
+        // 커서값이 바뀔 때(변화가 감지 될 때) 실행
+        nextCursor
+            .flatMap {_ in
+                APIManager.shared.readPost(next: nextCursorItem, limit: "4", productId: "Five_Feed")
+            }
+            .subscribe(with: self) { owner , response in
+
+                switch response {
+                case .success(let success) :
+                    
+                    var itemArray = try! feedData.value() //원래 배열
+                    itemArray.append(contentsOf: success.data )
+                    feedData.onNext(itemArray) // 원래 + 새로움 = 갈아
+                    
+                    nextCursorItem = success.nextCursor // 페이지네이션에 쓸 넥스트 커서를 저장해
+                
+                case .failure(let failure) :
+                    print ("=== 커서값 변경 실패:",failure)
+                }
+                                         
+            }
+            .disposed(by: disposeBag)
+                
+        
+        //MARK: - 게시글 갱신 (컨텐츠 작성 후)
         
         input.refresh
             .flatMap {
-                APIManager.shared.readPost(next: "", limit: "", productId: "Five_Feed")
+                APIManager.shared.readPost(next: "", limit: "4", productId: "Five_Feed")
             }
             .subscribe(with: self) { owner, value in
                 switch value {
                 case .success(let response):
-                    print("===feedVC",response)
-                    FeedData.onNext(response.data)
-                    nextCursor.onNext(response.nextCursor)
+                    print("===갱신 feedVC",response)
+                    feedData.onNext(response.data)
+
                     
                 case .failure(let error):
                     print("\(error)")
@@ -52,14 +96,15 @@ class FeedViewModel {
             }
             .disposed(by: disposeBag)
         
+        
+        //MARK: - 게시글 조회 (최초)
         //네트워크 통신
-        APIManager.shared.readPost(next: "", limit: "", productId: "Five_Feed")
+        APIManager.shared.readPost(next: "", limit: "4", productId: "Five_Feed")
             .subscribe(with: self) { owner, value in
                 switch value {
                 case .success(let response):
-                    print("===feedVC",response)
-                    FeedData.onNext(response.data)
-                    nextCursor.onNext(response.nextCursor)
+                    print("===최초 feedVC",response)
+                    feedData.onNext(response.data)
                     
                 case .failure(let error):
                     print("\(error)")
@@ -67,7 +112,7 @@ class FeedViewModel {
             }
             .disposed(by: disposeBag)
         
-        return Output(items: FeedData, nextCursor: nextCursor)
+        return Output(items: feedData)
     }
     
     
