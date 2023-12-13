0077f10c46537b5e22a50419ef3fcd033510a65c
Seungyeon <122261047+989ksy@users.noreply.github.com>
2023-12-13 21:24:16 +0900

    [Feat] PostCollectionViewCell UI + Layout

---
+++ Five/Five/Post/Cell/PostCollectionViewCell.swift
@@ -0,0 +1,68 @@
+//
+//  PostCollectionViewCell.swift
+//  Five
+//
+//  Created by Seungyeon Kim on 12/13/23.
+//
+
+import UIKit
+import SnapKit
+
+class PostCollectionViewCell : BaseCollectionViewCell {
+    
+    static let identifier = "PostCollectionViewCell"
+    
+    let uploadedImaegView = {
+        let view = UIImageView()
+        view.image = UIImage(systemName: "heart")
+        view.contentMode = .scaleAspectFit
+        return view
+    }()
+    
+    let countBackView = {
+        let view = UIView()
+        view.backgroundColor = .darkGray.withAlphaComponent(0.7)
+        view.layer.cornerRadius = 8
+        return view
+    }()
+    
+    let countLabel = {
+        let view = UILabel()
+        view.textColor = .white
+        view.font = CustomFont.mediumGmarket12
+        view.textAlignment = .center
+        return view
+    }()
+    
+    
+    override func configureView() {
+        
+        addSubview(uploadedImaegView)
+        uploadedImaegView.backgroundColor = .brown
+        addSubview(countBackView)
+        countBackView.addSubview(countLabel)
+        
+    }
+    
+    override func setConstraints() {
+        
+        uploadedImaegView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+        countBackView.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(14)
+            make.height.equalTo(28)
+            make.width.equalTo(43)
+            make.trailing.equalToSuperview().inset(12)
+        }
+        countLabel.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.centerX.equalToSuperview()
+//            make.top.equalToSuperview().offset(11)
+//            make.trailing.equalToSuperview().inset(14)
+//            make.height.equalTo(20)
+        }
+        
+    }
+    
+}
