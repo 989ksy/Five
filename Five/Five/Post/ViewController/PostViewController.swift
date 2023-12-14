9dbc20c0b121af948064264682f126730a310100
Seungyeon <122261047+989ksy@users.noreply.github.com>
2023-12-14 15:04:38 +0900

    [Feat] PostVC 값전달 완료 + 삭제 버튼 세팅

--- Five/Five/Post/ViewController/PostViewController.swift
+++ Five/Five/Post/ViewController/PostViewController.swift
@@ -14,7 +14,6 @@ final class PostViewController : BaseViewController {
     var transitedData = BehaviorRelay(value: ReadData(likes: [], image: [], id: "", creator: Creator.init(id: "", nick: ""), time: "", content: "", productID: ""))
     
     let mainView = PostView()
-    
     let disposeBag = DisposeBag()
     
     override func loadView() {
@@ -31,10 +30,11 @@ final class PostViewController : BaseViewController {
         mainView.imageCollectionView.dataSource = self
         mainView.imageCollectionView.delegate = self
         
-//        bindData()
+        bindData()
+        setMenu()
     }
     
-    /// 네비게이션바 타이틀 설정 (닉네임 - 게시글)
+    /// 네비게이션바 타이틀 위치 설정 (닉네임 - 게시글)
     override func viewWillLayoutSubviews() {
         super.viewWillLayoutSubviews()
         
@@ -47,21 +47,48 @@ final class PostViewController : BaseViewController {
         }
     }
     
+    @objc func buttonHandler(_ sender: UIButton) {
+        
+        }
+    
     
     func bindData() {
-        
         transitedData
             .bind(with: self) { owner, response in
                 
+                //닉네임 (네비게이션바)
+                if let titleLabel = owner.mainView.titleStackView.arrangedSubviews[0] as? UILabel {
+                    titleLabel.text = "@\(response.creator.nick)"
+                }
                 //닉네임 (프로필바)
                 owner.mainView.nickLabel.text = response.creator.nick
                 //내용
                 owner.mainView.contentLabel.text = response.content
                 //날짜
-                owner.mainView.dateLabel.text = response.time
-                
+                owner.mainView.dateLabel.customDateFormat(initialText: response.time)
             }
             .disposed(by: disposeBag)
+        
+    }
+    
+    func setMenu() {
+        
+        var items: [UIAction] {
+            let delete = UIAction(
+                title: "삭제",
+                image: UIImage(named: "Trash_Custom"),
+                handler: { [unowned self] _ in
+                    print("tapped?")
+                })
+            let Items = [delete]
+            return Items
+        }
+        
+        let menu = UIMenu(title: "",
+                          children: items)
+        
+        mainView.optionButton.menu = menu
+        mainView.optionButton.showsMenuAsPrimaryAction = true
         
     }
     
@@ -70,21 +97,33 @@ final class PostViewController : BaseViewController {
 }
 
 
+//MARK: - CollectionView 설정
+
 extension PostViewController : UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    //MARK: - 이미지
+    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 4
+        return transitedData.value.image.count
     }
+    
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PostCollectionViewCell", for: indexPath) as? PostCollectionViewCell else { return UICollectionViewCell() }
         
-        cell.countLabel.text = "\(indexPath.row + 1)/\(indexPath.count)"
+        cell.countLabel.text = "\(indexPath.row + 1)/\(transitedData.value.image.count)"
         
+        let imageUrls = transitedData.value.image[indexPath.item]
+
+           let imageUrlString = String(imageUrls)
+
+           let pic = URL(string: "\(BaseURL.base)" + imageUrlString)
+
+           cell.uploadedImaegView.loadImage(from: pic!, placeHolderImage: UIImage(named: "personal"))
+
         return cell
     }
-    
-    
     
     
 }
