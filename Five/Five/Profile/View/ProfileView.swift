fc17c7dc8e1434e7f0df2eace1e99dea5ea2228e
Seungyeon <122261047+989ksy@users.noreply.github.com>
2023-12-17 16:31:36 +0900

    [Edit] ProfileView UI + Layout 수정

--- Five/Five/Profile/View/ProfileView.swift
+++ Five/Five/Profile/View/ProfileView.swift
@@ -10,210 +10,25 @@ import SnapKit
 
 class ProfileView : BaseView {
     
-    let backgroundView = {
-        let view = UIView()
-        view.backgroundColor = CustomColor.pointColor?.withAlphaComponent(0.6)
+    let ProfiletableView = {
+        let view = UITableView()
+        view.register(ProfileSecondCell.self, forCellReuseIdentifier: "ProfileSecondCell")
+        view.register(ProfileFirstCell.self, forCellReuseIdentifier: "ProfileFirstCell")
+        view.separatorStyle = .none
         return view
     }()
-    
-    let settingButton = {
-        let btn = UIButton()
-        btn.setImage(UIImage(named: "setting")?.withRenderingMode(.alwaysTemplate).withTintColor(.white), for: .normal)
-        btn.layer.cornerRadius = 16
-        btn.layer.borderWidth = 5
-        btn.layer.borderColor = CustomColor.backgroundColor?.cgColor
-        btn.tintColor = .darkGray
-        btn.backgroundColor = CustomColor.backgroundColor
-        return btn
-    }()
-    
-    let profileImage = {
-        let view = UIImageView()
-        view.contentMode = .scaleAspectFill
-        view.clipsToBounds = true
-        view.layer.cornerRadius = 45
-        view.layer.borderWidth = 5
-        view.layer.borderColor = CustomColor.backgroundColor?.cgColor
-        view.image = UIImage(named: "test_profile")
-        return view
-    }()
-    
-    let nicknameLabel = {
-        let label = UILabel()
-        label.text = "The대투수"
-        label.font = CustomFont.boldGmarket20
-        label.textColor = .black
-        return label
-    }()
-    
-    let followLabel = {
-        let label = UILabel()
-        label.text = "팔로우"
-        label.font = CustomFont.lightGmarket15
-        label.textColor = .black
-        return label
-    }()
-    
-    let followDataLabel = {
-        let label = UILabel()
-        label.text = "545,454"
-        label.font = CustomFont.mediumGmarket17
-        label.textColor = .black
-        label.textAlignment = .center
-        return label
-    }()
-    
-    let followingLabel = {
-        let label = UILabel()
-        label.text = "팔로잉"
-        label.font = CustomFont.lightGmarket15
-        label.textColor = .black
-        return label
-    }()
-    
-    let followingDataLabel = {
-        let label = UILabel()
-        label.text = "1,100"
-        label.font = CustomFont.mediumGmarket17
-        label.textColor = .black
-        label.textAlignment = .center
-        return label
-    }()
-    
-    let followButton = {
-        let btn = FollowUIButton()
-        return btn
-    }()
-    
-    let segmentedControl = {
-        let control = UnderlineSegmentedControl(items: ["Five","Fived"])
-        control.translatesAutoresizingMaskIntoConstraints = false
-        return control
-    }()
-    
-    let fiveView = {
-        let view = UICollectionView(frame: .zero, collectionViewLayout: configureCollectionLayout())
-        view.register(FiveCollectionViewCell.self, forCellWithReuseIdentifier: "FiveCollectionViewCell")
-        return view
-    }()
-    
-    let fivedView = {
-        let view = UIView()
-        view.backgroundColor = .blue
-        view.translatesAutoresizingMaskIntoConstraints = false
-        return view
-    }()
-    
-    static func configureCollectionLayout() -> UICollectionViewLayout {
-
-        let layout = UICollectionViewFlowLayout()
-          
-          let spacing: CGFloat = 3
-          let numberOfItemsInRow: CGFloat = 3
-  
-          let width = (UIScreen.main.bounds.width - (numberOfItemsInRow + 1) * spacing) / numberOfItemsInRow
-          layout.itemSize = CGSize(width: width, height: width)
-          
-          layout.minimumInteritemSpacing = spacing
-          layout.minimumLineSpacing = spacing
-          
-          layout.sectionInset = UIEdgeInsets(top: spacing, left: spacing, bottom: spacing, right: spacing)
-          
-          return layout
-        
-    }
-    
     
     override func configureView() {
-        addSubview(backgroundView)
-        addSubview(profileImage)
-        addSubview(settingButton)
-        addSubview(nicknameLabel)
-        addSubview(followLabel)
-        addSubview(followingLabel)
-        addSubview(followDataLabel)
-        addSubview(followingDataLabel)
-        addSubview(followButton)
-        addSubview(segmentedControl)
-        addSubview(fivedView)
-        addSubview(fiveView)
+        addSubview(ProfiletableView)
     }
     
     override func setConstraints() {
         
-        backgroundView.snp.makeConstraints { make in
-            make.top.equalToSuperview()
-            make.horizontalEdges.equalToSuperview()
-            make.height.equalTo(130)
-        }
-        profileImage.snp.makeConstraints { make in
+        ProfiletableView.snp.makeConstraints { make in
             make.top.equalTo(safeAreaLayoutGuide)
-            make.size.equalTo(90)
-            make.centerX.equalToSuperview()
+            make.horizontalEdges.bottom.equalToSuperview()
         }
-        
-        settingButton.snp.makeConstraints { make in
-            make.size.equalTo(32)
-            make.leading.equalTo(profileImage.snp.trailing).inset(30)
-            make.top.equalTo(backgroundView.snp.bottom).offset(10)
-        }
-        
-        nicknameLabel.snp.makeConstraints { make in
-            make.top.equalTo(profileImage.snp.bottom).offset(13)
-            make.centerX.equalToSuperview()
-            make.height.equalTo(22)
-        }
-        
-        followingDataLabel.snp.makeConstraints { make in
-            make.bottom.equalTo(backgroundView.snp.bottom).offset(-12)
-            make.height.equalTo(18)
-            make.leading.equalToSuperview().offset(4)
-            make.trailing.equalTo(profileImage.snp.leading).offset(-4)
-        }
-        
-        followingLabel.snp.makeConstraints { make in
-            make.bottom.equalTo(followingDataLabel.snp.top).offset(-8)
-            make.height.equalTo(15)
-            make.centerX.equalTo(followingDataLabel)
-        }
-        
-        followDataLabel.snp.makeConstraints { make in
-            make.bottom.equalTo(backgroundView.snp.bottom).offset(-12)
-            make.height.equalTo(18)
-            make.trailing.equalToSuperview().offset(-4)
-            make.leading.equalTo(profileImage.snp.trailing).offset(4)
-        }
-        
-        followLabel.snp.makeConstraints { make in
-            make.bottom.equalTo(followDataLabel.snp.top).offset(-8)
-            make.height.equalTo(15)
-            make.centerX.equalTo(followDataLabel)
-        }
-        
-        followButton.snp.makeConstraints { make in
-            make.top.equalTo(nicknameLabel.snp.bottom).offset(12)
-            make.height.equalTo(35)
-            make.width.equalTo(90)
-            make.centerX.equalTo(profileImage)
-        }
-        
-        segmentedControl.snp.makeConstraints { make in
-            make.top.equalTo(followButton.snp.bottom).offset(14)
-            make.horizontalEdges.equalToSuperview().inset(120)
-            make.height.equalTo(40)
-        }
-        
-        fiveView.snp.makeConstraints { make in
-            make.top.equalTo(segmentedControl.snp.bottom).offset(12)
-            make.horizontalEdges.equalToSuperview()
-            make.bottom.equalToSuperview()
-        }
-        
-        fivedView.snp.makeConstraints { make in
-            make.top.equalTo(segmentedControl.snp.bottom).offset(12)
-            make.horizontalEdges.equalToSuperview()
-            make.bottom.equalToSuperview()
-        }
+    
         
     }
     
