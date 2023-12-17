ebe69bd3f53aac42beeea2bd6c43b95eca8a3a69
Seungyeon <122261047+989ksy@users.noreply.github.com>
2023-12-18 00:26:55 +0900

    [Feat] 컨텐츠 추가하기 버튼 Extension으로 관리

---
+++ Five/Five/Extension + Share/UIButton/AddContentButton.swift
@@ -0,0 +1,33 @@
+//
+//  AddContentButton.swift
+//  Five
+//
+//  Created by Seungyeon Kim on 12/17/23.
+//
+
+import UIKit
+
+class AddContentButton : UIButton {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        set()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func set() {
+       setImage(UIImage(named: "customPlus")?.withTintColor(.white), for: .normal)
+        backgroundColor = CustomColor.pointColor
+        layer.cornerRadius = 30
+        layer.shadowColor = UIColor.gray.cgColor
+        layer.shadowOpacity = 0.3
+        layer.shadowOffset = CGSize.zero
+        layer.shadowRadius = 6
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+}
+
