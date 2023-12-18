182191edf1ded9df8ea11e41a617b42bc55888f3
Seungyeon <122261047+989ksy@users.noreply.github.com>
2023-12-18 21:25:46 +0900

    [Feat] CommentVC_ 셀 높이 동적으로 변화 구현

--- Five/Five/Comment/ViewController/CommentViewController.swift
+++ Five/Five/Comment/ViewController/CommentViewController.swift
@@ -10,16 +10,49 @@ import UIKit
 final class CommentViewController : BaseViewController {
     
     let mainView = CommentView()
-    
+
     override func loadView() {
         self.view = mainView
     }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = CustomColor.backgroundColor
+
+        mainView.commentTableView.delegate = self
+        mainView.commentTableView.dataSource = self
         
+        mainView.commentTableView.rowHeight = UITableView.automaticDimension
         
     }
+
+    
+    
+    
+}
+
+
+extension CommentViewController : UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "CommentTableViewCell", for: indexPath) as? CommentTableViewCell else {return UITableViewCell()}
+        
+        return cell
+        
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //선택 시 회색셀렉션 제거
+        tableView.reloadRows(at: [indexPath], with: .none)
+
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+           return tableView.rowHeight
+       }
+    
+    
     
 }
