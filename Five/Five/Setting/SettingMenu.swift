fea904f64fb1fcf71560604b304fd946a0ad7086
Seungyeon <122261047+989ksy@users.noreply.github.com>
2023-12-20 02:40:18 +0900

    [Feat] SettingMenu_ 고정된 정보값 struct 관리

---
+++ Five/Five/Setting/SettingMenu.swift
@@ -0,0 +1,18 @@
+//
+//  SettingMenu.swift
+//  Five
+//
+//  Created by Seungyeon Kim on 12/19/23.
+//
+
+import UIKit
+
+struct SettingMenu {
+    
+    let list : [Setting] = [
+        Setting(label: "닉네임"),
+        Setting(label: "이메일"),
+        Setting(label: "휴대폰 번호"),
+    ]
+    
+}
