874a577b10c693f202d13b476f822dd1e8d167f9
Seungyeon <122261047+989ksy@users.noreply.github.com>
2023-12-20 02:40:38 +0900

    [Design] SettingVC_ UI + Layout 수정

--- Five/Five/Setting/View/SettingView.swift
+++ Five/Five/Setting/View/SettingView.swift
@@ -14,126 +14,97 @@ class SettingView : BaseView {
         let view = UIImageView()
         view.contentMode = .scaleAspectFill
         view.clipsToBounds = true
-        view.layer.cornerRadius = 60
-        view.layer.borderWidth = 5
-        view.layer.borderColor = CustomColor.pointColor?.cgColor
-        view.image = UIImage(named: "test_profile")
+        view.layer.cornerRadius = 50
+        view.layer.borderWidth = 1
+        view.layer.borderColor = UIColor.systemGray4.cgColor
+        view.image = UIImage(named: "test_image")
         return view
     }()
     
     let imageChangeButton = {
-        let btn = UIButton()
-        btn.setTitle("프로필 사진 바꾸기", for: .normal)
-        btn.setTitleColor(.darkGray, for: .normal)
-        btn.titleLabel?.font = CustomFont.mediumGmarket15
-        btn.layer.cornerRadius = 5
-        btn.layer.borderColor = UIColor.systemGray4.cgColor
-        btn.layer.borderWidth = 1
+        let btn = SettingButton()
+        btn.layer.borderColor = CustomColor.backgroundColor?.cgColor
+        btn.layer.borderWidth = 3
         return btn
     }()
     
-    let plainLine = {
-        let view = LineView()
+    let settingTableView = {
+        let view = UITableView()
+        view.layer.cornerRadius = 18
+        view.layer.borderColor = UIColor.systemGray4.cgColor
+        view.layer.borderWidth = 1
+        view.backgroundColor = CustomColor.backgroundColor
+        view.separatorStyle = .none
+        view.register(SettingTableCell.self, forCellReuseIdentifier: "SettingTableCell")
         return view
     }()
     
-    let nickTitleLabel = {
+    let buttonStackView = {
+        let view = UIStackView()
+        view.distribution = .equalSpacing
+        view.axis = .horizontal
+        return view
+    }()
+    
+    let logoutButton = {
+        let btn = OutButton()
+        btn.setTitle("로그아웃", for: .normal)
+        return btn
+    }()
+    
+    let seperatorLabel = {
         let view = UILabel()
-        view.text = "닉네임"
-        view.font = CustomFont.mediumGmarket15
-        view.textColor = .black
-        return view
-    }()
-    
-    let nicknameTextField = {
-        let txtfield = UITextField()
-        txtfield.borderStyle = .none
-        txtfield.placeholder = "닉네임을 입력해주세요."
-        txtfield.font = CustomFont.mediumGmarket15
-        return txtfield
-    }()
-    
-    let nickTxtFieldLine = {
-        let view = LineView()
-        return view
-    }()
-    
-    let plainLine2 = {
-        let view = LineView()
+        view.text = "|"
+        view.font = CustomFont.mediumGmarket13
+        view.textColor = .systemGray4
         return view
     }()
     
     let withdrawButton = {
-        let btn = UIButton()
-        btn.setTitle("계정 탈퇴하기", for: .normal)
-        btn.setTitleColor(.systemGray2, for: .normal)
-        btn.titleLabel?.font = CustomFont.mediumGmarket15
-        btn.layer.borderColor = UIColor.systemGray4.cgColor
-        btn.layer.borderWidth = 1
-        btn.layer.cornerRadius = 5
+        let btn = OutButton()
+        btn.setTitle("회원탈퇴", for: .normal)
         return btn
     }()
+    
+//
     
     
     override func configureView() {
         addSubview(profileImageView)
         addSubview(imageChangeButton)
-        addSubview(plainLine)
-        addSubview(nickTitleLabel)
-        addSubview(nicknameTextField)
-        addSubview(nickTxtFieldLine)
-        addSubview(plainLine2)
-        addSubview(withdrawButton)
+        addSubview(settingTableView)
+        addSubview(buttonStackView)
+        buttonStackView.addArrangedSubview(logoutButton)
+        buttonStackView.addArrangedSubview(seperatorLabel)
+        buttonStackView.addArrangedSubview(withdrawButton)
+
     }
     
     override func setConstraints() {
         profileImageView.snp.makeConstraints { make in
-            make.top.equalTo(safeAreaLayoutGuide).offset(27)
-            make.size.equalTo(120)
+            make.top.equalTo(safeAreaLayoutGuide).offset(34)
+            make.size.equalTo(100)
             make.centerX.equalToSuperview()
         }
         
         imageChangeButton.snp.makeConstraints { make in
-            make.top.equalTo(profileImageView.snp.bottom).offset(18)
-            make.centerX.equalToSuperview()
-            make.height.equalTo(34)
-            make.width.equalTo(140)
+            make.leading.equalTo(profileImageView.snp.trailing).inset(28)
+            make.size.equalTo(32)
+            make.bottom.equalTo(profileImageView.snp.bottom).inset(4)
         }
         
-        plainLine.snp.makeConstraints { make in
-            make.height.equalTo(1)
-            make.top.equalTo(imageChangeButton.snp.bottom).offset(21)
-            make.horizontalEdges.equalToSuperview().inset(10)
+        settingTableView.snp.makeConstraints { make in
+            make.top.equalTo(profileImageView.snp.bottom).offset(34)
+            make.horizontalEdges.equalToSuperview().inset(14)
+            make.height.equalTo(240)
         }
         
-        nickTitleLabel.snp.makeConstraints { make in
-            make.centerY.equalTo(nicknameTextField)
-            make.height.equalTo(18)
-            make.leading.equalToSuperview().offset(26)
-            
+        buttonStackView.snp.makeConstraints { make in
+            make.bottom.equalTo(safeAreaLayoutGuide).inset(60)
+            make.height.equalTo(17)
+            make.horizontalEdges.equalToSuperview().inset(140)
         }
-        
-        nicknameTextField.snp.makeConstraints { make in
-            make.top.equalTo(plainLine.snp.bottom).offset(18)
-            make.height.equalTo(34)
-            make.leading.equalToSuperview().offset(90)
-            make.trailing.equalToSuperview().inset(23)
-        }
-        
-        nickTxtFieldLine.snp.makeConstraints { make in
-            make.height.equalTo(1)
-            make.bottom.equalTo(nicknameTextField)
-            make.leading.equalToSuperview().offset(90)
-            make.trailing.equalToSuperview().inset(23)
-        }
-        
-        withdrawButton.snp.makeConstraints { make in
-            make.bottom.equalToSuperview().offset(-15)
-            make.horizontalEdges.equalToSuperview().inset(8)
-//            make.width.equalTo(160) //120
-            make.height.equalTo(50)
-            
-        }
+
         
     }
     
