d43369fe8a23dbb284e6a2e189befbb28666fb43
Seungyeon <122261047+989ksy@users.noreply.github.com>
2023-12-19 23:01:47 +0900

    [Feat] CommentVC_ 댓글 입력 구현

--- Five/Five/Comment/ViewController/CommentViewController.swift
+++ Five/Five/Comment/ViewController/CommentViewController.swift
@@ -6,24 +6,71 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 final class CommentViewController : BaseViewController {
     
     let mainView = CommentView()
-
+    let disposeBag = DisposeBag()
+        
+    var commentList : [CreateCommentResponse]?
+    
+    var postId : String?
+    
     override func loadView() {
         self.view = mainView
     }
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        //테이블뷰 설정
         mainView.commentTableView.delegate = self
         mainView.commentTableView.dataSource = self
-        
         mainView.commentTableView.rowHeight = UITableView.automaticDimension
         
+        bind()
+        
     }
+    
+    func bind() {
+        
+        guard let id = postId else { return }
+        
 
+        mainView.sentButton
+            .rx
+            .tap
+            .throttle(.seconds(2), scheduler: MainScheduler.instance)
+            .withLatestFrom(mainView.commentTextField.rx.text.orEmpty, resultSelector: { _, query in
+                return query
+            })
+            .flatMap{
+                APIManager.shared.createComment(id: id, content: $0 )
+            }
+            .subscribe(with: self, onNext: { owner, response in
+                switch response {
+                case .success(let response):
+                        
+                    self.commentList?.insert(response, at: 0)
+                    owner.mainView.commentTextField.text = ""
+                    
+                    NotificationCenter.default.post(name: NSNotification.Name("needToUpdate"), object: nil)
+                    
+                    owner.mainView.commentTableView.reloadData()
+                                                            
+                case .failure(let failure):
+                    print(failure.errorDescription!)
+                }
+            })
+            .disposed(by: disposeBag)
+        
+        
+        
+        
+        
+    }
+    
     
     
     
@@ -32,13 +79,28 @@ final class CommentViewController : BaseViewController {
 
 extension CommentViewController : UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+                
+        return commentList?.count ?? 0
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "CommentTableViewCell", for: indexPath) as? CommentTableViewCell else {return UITableViewCell()}
+                
+        let data = commentList?[indexPath.row]
         
+        cell.nicknameLabel.text = data?.creator.nick//닉네임
+        cell.commentLabel.text = data?.content //내용
+        cell.dateLabel.customDateFormat2(initialText: data?.time ?? "nil")//날짜
+        
+        //작성자 마크 표시
+        if data?.creator.id != KeychainStorage.shared.userID! {
+            cell.writerLabel.isHidden = true
+        } else {
+            cell.writerLabel.isHidden = false
+        }
+
+           
         return cell
         
     }
