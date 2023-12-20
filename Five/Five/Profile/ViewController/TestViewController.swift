6cd9d376c6853b3aff464eec02a29c6b7babcbfc
Seungyeon <122261047+989ksy@users.noreply.github.com>
2023-12-21 00:35:30 +0900

    [Test]

--- Five/Five/Profile/ViewController/TestViewController.swift
+++ Five/Five/Profile/ViewController/TestViewController.swift
@@ -5,4 +5,29 @@
 //  Created by Seungyeon Kim on 12/20/23.
 //
 
-import Foundation
+import UIKit
+import SnapKit
+
+class TestViewController: BaseViewController {
+    
+    let tableview = UITableView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func configureView() {
+        view.addSubview(tableview)
+        tableview.backgroundColor = .gray
+    }
+    
+    override func setConstraints() {
+        
+        tableview.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide)
+            make.horizontalEdges.bottom.equalToSuperview()
+        }
+        
+    }
+    
+}
